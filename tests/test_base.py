5ecbd494d495c57a02a62fd4b830b80854e83223
Manuel Dominic <ematembu2@gmail.com>
2018-11-28 14:00:17 +0300

    add get all redflags incindence

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,3 +1,4 @@
+from flask import json
 import unittest
 from IReporter_API.app.app import app
 
@@ -16,9 +17,40 @@ class TestCase(unittest.TestCase):
     def test_index(self):
         response = self.client.get('/')
         data = response.data.decode()
-        message = "Hello Ireporter"
-        self.assertEqual(data,message)
+        message = {'Message':"Hello Ireporter"}
+        self.assertEqual(json.loads(data),message)
 
+    def test_get_all_redflags(self):
+        response = self.client.get('/redflags')
+        data = response.data.decode()
+        message = {"status": 200, "data": {
+            "data": [
+            {
+            "comment": "Arnold was caught stilling jack fruit in hassan's Garden",
+            "createdBy": 2,
+            "createdOn": "Wed, 28 Nov 2018 13:54:02 GMT",
+            "images": "1.jpeg",
+            "incidentId": 1,
+            "location": "0.39737 , 9.38974",
+            "status": "draft",
+            "type": "redflag",
+            "videos": "1.gif"
+            },
+            {
+            "comment": "Hussien knocked moses's cow along masaka road, he was drank",
+            "createdBy": 2,
+            "createdOn": "Wed, 28 Nov 2018 13:54:02 GMT",
+            "images": "3.jpeg",
+            "incidentId": 3,
+            "location": "0.39737 , 9.38974",
+            "status": "draft",
+            "type": "redflag",
+            "videos": "3.gif"
+            }
+            ],
+            "status": 200
+            }}
+        self.assertEqual(json.loads(data),message)
 
 
 if __name__ == '__main__':
