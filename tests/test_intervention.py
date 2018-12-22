7a454cebd3ecf0ea7ab7d08b4d9abc531e9c3902
ManuelDominic <ematembu2@gmail.com>
2018-12-23 01:02:15 +0300

    Modify tests

--- tests/test_intervention.py
+++ tests/test_intervention.py
@@ -1,120 +1,120 @@
-# import json
-# import unittest
-# import os
-# import sys
+import json
+import unittest
+import os
+import sys
 
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
-# from api.views.intervention_view import intervention_table
-# from api.app import app
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+from api.views.intervention_view import intervention_table
+from api.app import app
 
-# new_intervention = {
-#     "comment": "Jinja bridge needs construction",
-#     "createdBy": 3,
-#     "images": "1.jpeg",
-#     "locationLong": "6.66666",
-#     "locationLat": "7.7777",
-#     "type": "redflag",
-#     "videos": "1.gif"
+new_intervention = {
+    "comment": "Jinja bridge needs construction",
+    "createdBy": 3,
+    "images": "1.jpeg",
+    "locationLong": "6.66666",
+    "locationLat": "7.7777",
+    "type": "redflag",
+    "videos": "1.gif"
 
-# }
-# new_intervention_response = {
-#     "data": [
-#         {
-#             "id": 4,
-#             "message": "Created intervention record"
-#         }
-#     ],
-#     "status": 200
-# }
-# new_location = {
-#     "locationLong": "8.555555",
-#     "locationLat": "5.88289"
-# }
-# new_comment = {"comment": "Sorry!, error information"}
-
-
-# def get_incidents_by_type(incident_type):
-#     all_incidents = []
-
-#     for incident in intervention_table:
-#         if incident.type == incident_type:
-#             all_incidents.append(
-#                 {
-#                     "comment": incident.comment,
-#                     "createdBy": incident.createdBy,
-#                     "createdOn": incident.createdOn,
-#                     "images": incident.images,
-#                     "incidentId": incident.incidentId,
-#                     "location": " ".join([incident.locationLong, ',', incident.locationLat]),
-#                     "status": incident.status,
-#                     "type": incident.type,
-#                     "videos": incident.videos
-#                 }
-#             )
-#     return all_incidents
+}
+new_intervention_response = {
+    "data": [
+        {
+            "id": 4,
+            "message": "Created intervention record"
+        }
+    ],
+    "status": 200
+}
+new_location = {
+    "locationLong": "8.555555",
+    "locationLat": "5.88289"
+}
+new_comment = {"comment": "Sorry!, error information"}
 
 
-# class TestIntervention(unittest.TestCase):
+def get_incidents_by_type(incident_type):
+    all_incidents = []
 
-#     def setUp(self):
-#         app.config['TESTING'] = True
-#         app.config['DEBUG'] = False
-#         self.app = app.test_client()
-
-#     def test_get_all_intervention(self):
-#         response = self.app.get('/api/v1/intervention')
-#         data = response.data.decode()
-#         message = {"data": get_incidents_by_type("intervention"), "status": 200}
-#         self.assertEqual(len(json.loads(data)), len(message))
-
-#     def test_get_specific_intervention(self):
-#         response = self.app.get('/api/v1/intervention/1')
-#         data = response.data.decode()
-#         message = {
-#             "data": {
-#                 "comment": "Mbale highway needs construction",
-#                 "createdBy": 2,
-#                 "createdOn": "Fri, 30 Nov 2018 13:09:32 GMT",
-#                 "images": "1.jpeg",
-#                 "incidentId": 1,
-#                 "location": "0.33737 , 5.38974",
-#                 "status": "draft",
-#                 "type": "intervention",
-#                 "videos": "1.gif"
-#             },
-#             "status": 200
-#         }
-#         self.assertEqual(json.loads(data), message)
-
-#     def test_create_intervention(self):
-#         response = self.app.post('/api/v1/intervention', content_type="application/json",
-#                                  data=json.dumps(new_intervention))
-#         data = response.data.decode()
-#         self.assertEqual(json.loads(data), new_intervention_response)
-
-#     def test_update_intervention_location(self):
-#         response = self.app.patch('/api/v1/intervention/2/location', content_type="application/json",
-#                                   data=json.dumps(new_location))
-#         data = response.data.decode()
-#         message = {"data": [{"id": 2, "message": "Updated intervention record's location"}],
-#                    "status": 200}
-#         self.assertEqual(json.loads(data), message)
-
-#     def test_update_intervention_comment(self):
-#         response = self.app.patch('/api/v1/intervention/2/comment', content_type="application/json",
-#                                   data=json.dumps(new_comment))
-#         data = response.data.decode()
-#         message = {"data": [{"id": 2, "message": "Updated intervention record's comment"}],
-#                    "status": 200}
-#         self.assertEqual(json.loads(data), message)
-
-#     def test_delete_intervention(self):
-#         response = self.app.delete('/api/v1/intervention/4')
-#         data = response.data.decode()
-#         message = {"data": [{"id": 4, "message": "intervention record has been deleted"}],
-#                    "status": 200}
-#         self.assertEqual(json.loads(data), message)
+    for incident in intervention_table:
+        if incident.type == incident_type:
+            all_incidents.append(
+                {
+                    "comment": incident.comment,
+                    "createdBy": incident.createdBy,
+                    "createdOn": incident.createdOn,
+                    "images": incident.images,
+                    "incidentId": incident.incidentId,
+                    "location": " ".join([incident.locationLong, ',', incident.locationLat]),
+                    "status": incident.status,
+                    "type": incident.type,
+                    "videos": incident.videos
+                }
+            )
+    return all_incidents
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+class TestIntervention(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+    def test_get_all_intervention(self):
+        response = self.app.get('/api/v1/intervention')
+        data = response.data.decode()
+        message = {"data": get_incidents_by_type("intervention"), "status": 200}
+        self.assertEqual(len(json.loads(data)), len(message))
+
+    def test_get_specific_intervention(self):
+        response = self.app.get('/api/v1/intervention/1')
+        data = response.data.decode()
+        message = {
+            "data": {
+                "comment": "Mbale highway needs construction",
+                "createdBy": 2,
+                "createdOn": "Fri, 30 Nov 2018 13:09:32 GMT",
+                "images": "1.jpeg",
+                "incidentId": 1,
+                "location": "0.33737 , 5.38974",
+                "status": "draft",
+                "type": "intervention",
+                "videos": "1.gif"
+            },
+            "status": 200
+        }
+        self.assertEqual(json.loads(data), message)
+
+    def test_create_intervention(self):
+        response = self.app.post('/api/v1/intervention', content_type="application/json",
+                                 data=json.dumps(new_intervention))
+        data = response.data.decode()
+        self.assertEqual(json.loads(data), new_intervention_response)
+
+    def test_update_intervention_location(self):
+        response = self.app.patch('/api/v1/intervention/2/location', content_type="application/json",
+                                  data=json.dumps(new_location))
+        data = response.data.decode()
+        message = {"data": [{"id": 2, "message": "Updated intervention record's location"}],
+                   "status": 200}
+        self.assertEqual(json.loads(data), message)
+
+    def test_update_intervention_comment(self):
+        response = self.app.patch('/api/v1/intervention/2/comment', content_type="application/json",
+                                  data=json.dumps(new_comment))
+        data = response.data.decode()
+        message = {"data": [{"id": 2, "message": "Updated intervention record's comment"}],
+                   "status": 200}
+        self.assertEqual(json.loads(data), message)
+
+    def test_delete_intervention(self):
+        response = self.app.delete('/api/v1/intervention/4')
+        data = response.data.decode()
+        message = {"data": [{"id": 4, "message": "intervention record has been deleted"}],
+                   "status": 200}
+        self.assertEqual(json.loads(data), message)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,82 +1,82 @@
-# import json
-# import unittest
-# import os
-# import sys
+import json
+import unittest
+import os
+import sys
 
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
-# from api.views.user_view import user_bp
-# from api.models.user_model import User
-# from api.helpers.auth import encode_token
-# from api.app import app
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+from api.views.user_view import user_bp
+from api.models.user_model import User
+from api.helpers.auth import encode_token
+from api.app import app
 
-# new_user = {
-#     "email": "ematembu@gmail.com",
-#     "firstName": "manuel",
-#     "lastName": "Dominic",
-#     "otherName": "highway",
-#     "password": "manuel123",
-#     "phoneNumber": "256700701616",
-#     "userName": "mats"
-# }
-# new_msg = {
-#     "email": "ematembu@gmail.com",
-#     "firstName": "manuel",
-#     "lastName": "Dominic",
-#     "otherName": "highway",
-#     "userId": 2,
-#     "phoneNumber": "256700701616",
-#     "userName": "mats",
-#     "isAdmin":False
-# }
+new_user = {
+    "email": "ematembu@gmail.com",
+    "firstName": "manuel",
+    "lastName": "Dominic",
+    "otherName": "highway",
+    "password": "manuel123",
+    "phoneNumber": "256700701616",
+    "userName": "mats"
+}
+new_msg = {
+    "email": "ematembu@gmail.com",
+    "firstName": "manuel",
+    "lastName": "Dominic",
+    "otherName": "highway",
+    "userId": 2,
+    "phoneNumber": "256700701616",
+    "userName": "mats",
+    "isAdmin":False
+}
 
-# users_list = {
-#     "email": "ematembu@gmail.com",
-#     "firstName": "manuel",
-#     "lastName": "Dominic",
-#     "otherName": "highway",
-#     "password": "manuel123",
-#     "phoneNumber": "256700701616",
-#     "userName": "mats"
-# }
+users_list = {
+    "email": "ematembu@gmail.com",
+    "firstName": "manuel",
+    "lastName": "Dominic",
+    "otherName": "highway",
+    "password": "manuel123",
+    "phoneNumber": "256700701616",
+    "userName": "mats"
+}
 
-# new_user_response = {
-#     "status": 201,
-#     "message": "Successfully registered",
-#     "users": new_msg}
+new_user_response = {
+    "status": 201,
+    "message": "Successfully registered",
+    "users": new_msg}
 
-# login_user = {
-#     "email": "admin@ireporter.com",
-#     "password": "admin123"
-# }
+login_user = {
+    "email": "admin@ireporter.com",
+    "password": "admin123"
+}
 
-# login_user_response = {
-#     "Token": encode_token(User.userId),
-#     "message": "Successfully logged In"}
-
-
-# class TestUser(unittest.TestCase):
-
-#     def setUp(self):
-#         app.config['TESTING'] = True
-#         app.config['DEBUG'] = False
-#         self.app = app.test_client()
-
-#     def test_sign_up(self):
-#         response = self.app.post('/api/v1/auth/signup', content_type="application/json", data=json.dumps(new_user))
-#         data = response.data.decode()
-#         self.assertEqual(json.loads(data), new_user_response)
-
-#     def test_login(self):
-#         response = self.app.post('/api/v1/users', content_type="application/json", data=json.dumps(login_user))
-#         data = response.data.decode()
-#         self.assertEqual(json.loads(data), login_user_response)
-
-#     def test_get_users(self):
-#         response = self.app.get('/api/v1/auth/login')
-#         data = response.data.decode()
-#         message = {"status": 200, "users": users_list}
-#         self.assertEqual(json.loads(data), message)
+login_user_response = {
+    "Token": encode_token(User.userId),
+    "message": "Successfully logged In"}
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+class TestUser(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+    def test_sign_up(self):
+        response = self.app.post('/api/v1/auth/signup', content_type="application/json", data=json.dumps(new_user))
+        data = response.data.decode()
+        self.assertEqual(json.loads(data), new_user_response)
+
+    def test_login(self):
+        response = self.app.post('/api/v1/users', content_type="application/json", data=json.dumps(login_user))
+        data = response.data.decode()
+        self.assertEqual(json.loads(data), login_user_response)
+
+    def test_get_users(self):
+        response = self.app.get('/api/v1/auth/login')
+        data = response.data.decode()
+        message = {"status": 200, "users": users_list}
+        self.assertEqual(json.loads(data), message)
+
+
+if __name__ == '__main__':
+    unittest.main()
