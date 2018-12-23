7ca815022d1983b1e57c20d22e41883f49d45a51
ManuelDominic <ematembu2@gmail.com>
2018-12-23 11:41:32 +0300

    Edit imports in app.py file

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from api.views.intervention_view import intervention_bp
 from api.views.redflag_view import redflag_bp
 from api.views.user_view import user_bp
-from api.views.incident_view import incident_bp
+# from api.views.incident_view import incident_bp
 
 
 app = Flask(__name__)
